8d94d75da06257b9ffba81bed027f8232411aff1
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-10-27 14:26:11 +0100

    Add Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
