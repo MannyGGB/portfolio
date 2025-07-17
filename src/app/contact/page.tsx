7547c4d2a135a602f37ffad9023701827af3f96f
Manuel <manugonzalez.tech@gmail.com>
2025-07-17 11:01:36 +0100

    fix(file tree): replace resume route with contact route

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "MannyGGB - Resume",
-  description: "A resume page for MannyGGB",
+  title: "MannyGGB - Contact",
+  description: "A contact page for MannyGGB",
 };
 
 export default function ResumePage() {
